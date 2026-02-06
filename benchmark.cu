f321440db7c60d7a024e6e088ec54db3c3cbb5b3
ayush <99374392+ayush-os@users.noreply.github.com>
2026-02-05 19:31:02 -0800

    keep blocksPerGrid for warmup

--- benchmark.cu
+++ benchmark.cu
@@ -43,6 +43,7 @@ int main() {
 
     // Kernel Launch Configuration
     int threadsPerBlock = 256;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
     int streamBlocks = (ELEMS_PER_STREAM + threadsPerBlock - 1) / threadsPerBlock;
     
     cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
